1b7fc4e1803f3535f25adcaeebe8cf49a4d8e510
Nilsferd <nilskrannig@gmail.com>
2020-06-10 17:41:36 +0200

    add firebase for android

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -31,6 +31,7 @@ const kMessageContainerDecoration = BoxDecoration(
 );
 
 const kInputDecoration = InputDecoration(
+  hintText: 'Enter a value',
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   border: OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(32.0)),
